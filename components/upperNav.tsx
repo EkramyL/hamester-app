939d0325cd49fe67a8569accd6ba656c29b667d5
Ekramy Latief <ekramyl3000@gmail.com>
2024-07-09 17:39:46 +0300

    get first structure

---
+++ components/upperNav.tsx
@@ -0,0 +1,13 @@
+import Hamster from "@/icons/Hamster";
+import React from "react";
+
+const UpperNav = () => {
+  return (
+    <div className="cent flex items-center gap-2 p-2">
+      <Hamster />
+      <p className="text-sm"> Ekramy Latief (CEO)</p>
+    </div>
+  );
+};
+
+export default UpperNav;
